0f988dc625e59e64572e82121b1ae749d25ce6bc
dobermensch <dobermensch@gmail.com>
2022-03-17 18:44:39 +0800

    add: twitter tracking

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-css-tags */
 /* eslint-disable @next/next/no-img-element */
 import React, {useState} from "react";
@@ -31,6 +32,17 @@ export default function Home() {
           data-n-g=""
         />
         <noscript data-n-css=""></noscript>
+
+        {/* <!-- Twitter universal website tag code --> */}
+        <script>
+          !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+          },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+          a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+          {/* // Insert Twitter Pixel ID and Standard Event data below */}
+          twq('init','o84pb');
+          twq('track','PageView');
+        </script>
+        {/* <!-- End Twitter universal website tag code --> */}
       </Head>
 
     <div id="__next" data-reactroot="">
