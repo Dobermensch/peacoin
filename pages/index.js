acf061e3831aaac4935c066c08cc86bc9c390ead
dobermensch <dobermensch@gmail.com>
2022-03-17 18:54:48 +0800

    fix: comment twitter tracking

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 // import styles from '../styles/Home.module.css'
+// <!-- Twitter universal website tag code -->
+/* <script> */
+/* !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+{/* // Insert Twitter Pixel ID and Standard Event data below
+twq('init','o84pb');
+twq('track','PageView');
+*/
+// </script>
+/* <!-- End Twitter universal website tag code --> */
 
 export default function Home() {
   const [page, setPage] = useState("");
@@ -32,17 +43,6 @@ export default function Home() {
           data-n-g=""
         />
         <noscript data-n-css=""></noscript>
-
-        {/* <!-- Twitter universal website tag code --> */}
-        <script>
-          !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-          },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
-          a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-          {/* // Insert Twitter Pixel ID and Standard Event data below */}
-          twq('init','o84pb');
-          twq('track','PageView');
-        </script>
-        {/* <!-- End Twitter universal website tag code --> */}
       </Head>
 
     <div id="__next" data-reactroot="">
